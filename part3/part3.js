949347563d7f73042e699fe39f549567ecd2295b
unknown <kthetroller@gmail.com>
2024-08-17 11:13:42 +0700

    let,tt+,bien

---
+++ part3/part3.js
@@ -0,0 +1,11 @@
+
+let c = 5;
+let d = 5.9;
+let x = 5;
+let y = 5;
+let z = 6;
+const x2 = 5;
+// x = 6 
+console.log(`Hello 'world' from "JavaScript"`, x == y, x2)
+//dấu `` >> "",'' 
+// '+' để ghép các biến 
